c1502b5c8c7f9d539290aded7203dc30e9f3542c
Taylor Hogan <taylorb.hogan@gmail.com>
2022-02-25 15:54:30 -0800

    Update environment variables

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   environment: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 8000,
-  mongoURI: 'mongodb+srv://personnel-admin:89txgNdBqz2YC01F@mypersonnel.feotd.mongodb.net/mypersonnel?retryWrites=true'
+  mongoURI: process.env.MONGOURI
 }
