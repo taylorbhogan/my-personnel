a27e73735b46c3589b2b11541979912f0301ccc9
Taylor Hogan <taylorb.hogan@gmail.com>
2022-03-23 15:09:26 -0700

    Implement lottie animation

--- my-personnel/src/components/AppUtils/LoadingContent.js
+++ my-personnel/src/components/AppUtils/LoadingContent.js
@@ -1,5 +1,25 @@
+import lottie from "lottie-web";
+import { useEffect, useRef } from "react";
+import animationData from "../../lotties/rotating-loading.json";
+
 const LoadingContent = () => {
-  return <div>Loading!!!</div>;
+  const lottieRef = useRef();
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: lottieRef.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData,
+    });
+  }, []);
+
+  return (
+    <div className="flex w-full justify-center items-center">
+      <div style={{ width: "500px", height: "200px" }} ref={lottieRef}></div>
+    </div>
+  );
 };
 
 export default LoadingContent;
