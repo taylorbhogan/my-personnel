9bb167959c1d5f53dd4e9737a4cf7a8a5efd51a7
Taylor Hogan <taylorb.hogan@gmail.com>
2022-03-23 16:10:44 -0700

    Implement loading gif

--- my-personnel/src/components/AppUtils/LoadingContent.js
+++ my-personnel/src/components/AppUtils/LoadingContent.js
@@ -1,5 +1,16 @@
+import loadingCircle from "../../images/loading-rotating.gif";
+
 const LoadingContent = () => {
-  return <div>LoadingContent</div>;
+  return (
+    <div
+    className="flex justify-center"
+    >
+      <img
+      style={{ width: "80px", height: "80px" }}
+      src={loadingCircle}
+      alt="loading circle" />
+    </div>
+  );
 };
 
 export default LoadingContent;
