4cf953843c9a78af75543f841291083824650011
Taylor Hogan <taylorb.hogan@gmail.com>
2022-03-24 16:16:16 -0700

    Solve return key form submission failure bug

--- my-personnel/src/components/AppUtils/ButtonCloseX.js
+++ my-personnel/src/components/AppUtils/ButtonCloseX.js
@@ -3,6 +3,7 @@ import { GrClose } from "react-icons/gr";
 const ButtonCloseX = ({ setFunction }) => {
   return (
     <button
+      type="button"
       onClick={() => setFunction(false)}
       className="absolute top-0 right-0 m-4"
     >
